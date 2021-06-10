08cc22613b1a0b77241b934ca6113f01666e2fa0
jbogensperger <jbogen@gmx.at>
2021-06-10 23:25:35 +0200

    2layer set-up

--- exp_design_eval_LM_2layer_part1.sh
+++ exp_design_eval_LM_2layer_part1.sh
@@ -14,33 +14,26 @@ fi
 
 
 #Dropout 0
- python main.py --exp_name LM_Exp_Layer2 --exp_id 201 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0  --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 205 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 206 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 210 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
-
-
-#Dropout 0.05
- python main.py --exp_name LM_Exp_Layer2 --exp_id 211 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.05  --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 215 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.05 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 216 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.05 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 220 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.05 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2 --exp_id 205 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2 --exp_id 210 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
 
 #Dropout 0.1
- python main.py --exp_name LM_Exp_Layer2 --exp_id 221 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.1  --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 225 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 226 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 230 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
-
-
-#Dropout 0.15
- python main.py --exp_name LM_Exp_Layer2 --exp_id 231 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.15  --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 235 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.15 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 236 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.15 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 240 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.15 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2 --exp_id 225 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2 --exp_id 230 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
 
 #Dropout 0.2
- python main.py --exp_name LM_Exp_Layer2 --exp_id 241 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.2  --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 245 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.2 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 246 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.2 --cut_strategy 1 --n_layer 2  && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 250 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.2 --cut_strategy 1 --n_layer 2
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2 --exp_id 245 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.2 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2 --exp_id 250 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.2 --cut_strategy 1 --n_layer 2
+
+
+#Dropout 0
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2_long --exp_id 205 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2_long --exp_id 210 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0 --cut_strategy 1 --n_layer 2  && \ 
+
+#Dropout 0.1
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2_long --exp_id 225 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2_long --exp_id 230 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.1 --cut_strategy 1 --n_layer 2  && \ 
+
+#Dropout 0.2
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2_long --exp_id 245 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.2 --cut_strategy 1 --n_layer 2  && \ 
+ CUDA_VISIBLE_DEVICES=1 python main.py --exp_name LM_Exp_Layer2_long --exp_id 250 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.2 --cut_strategy 1 --n_layer 2
--- exp_design_eval_LM_2layer_part2.sh
+++ exp_design_eval_LM_2layer_part2.sh
@@ -13,41 +13,36 @@ else
 	exit
 fi
 
-#Dropout 0.25 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 251 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.25  --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 255 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.25 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 256 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.25 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 260 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.25 --cut_strategy 1 --n_layer 2 && \
+#Dropout 0.3 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2 --exp_id 265 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2 --exp_id 270 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \
+
+
+
+#Dropout 0.4 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2 --exp_id 285 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2 --exp_id 290 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \
+
+
+#Dropout 0.5 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2 --exp_id 305 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.5 --cut_strategy 1 --n_layer 2 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2 --exp_id 310 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.5 --cut_strategy 1 --n_layer 2
+
 
 
 #Dropout 0.3 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 261 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.3  --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 265 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 266 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 270 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2_long --exp_id 265 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2_long --exp_id 270 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.3 --cut_strategy 1 --n_layer 2 && \
 
-#Dropout 0.35 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 271 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.35  --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 275 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.35 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 276 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.35 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 280 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.35 --cut_strategy 1 --n_layer 2 && \
+
 
 #Dropout 0.4 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 281 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.4  --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 285 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 286 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 290 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2_long --exp_id 285 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2_long --exp_id 290 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.4 --cut_strategy 1 --n_layer 2 && \
 
-#Dropout 0.45 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 291 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.45  --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 295 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.45 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 296 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.45 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 300 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.45 --cut_strategy 1 --n_layer 2 && \
 
 #Dropout 0.5 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 301 --num_tag 3 --model_name roberta-base --tgt_dm drugs  --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.5  --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 305 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.5 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 306 --num_tag 3 --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.5 --cut_strategy 1 --n_layer 2 && \ 
- python main.py --exp_name LM_Exp_Layer2 --exp_id 310 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 5 --dropout 0.5 --cut_strategy 1 --n_layer 2
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2_long --exp_id 305 --num_tag 3 --model_name roberta-base --ckpt LMs/ROBERTA/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.5 --cut_strategy 1 --n_layer 2 && \ 
+ CUDA_VISIBLE_DEVICES=2 python main.py --exp_name LM_Exp_Layer2_long --exp_id 310 --num_tag 3 --ckpt LMs/BERT/All/pytorch_model.bin --tgt_dm drugs --src_dm drugs --batch_size 4  --epoch 10 --dropout 0.5 --cut_strategy 1 --n_layer 2
  
  
